18bb39b602b881da183367925efc9504714dedc5
Meredith Underell <m.underell@gmail.com>
2013-09-28 16:10:00 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/* Module Dependencies */
+
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+// Firing up Express and telling it to use Jade and Stylus
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.end('Hi there!')
+})
+app.listen(3000)
+
+
+
+
+
+
+
+
