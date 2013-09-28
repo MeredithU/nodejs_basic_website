3ef2a3faf300a548362a64a2da8bec9750a2b018
Meredith Underell <m.underell@gmail.com>
2013-09-28 16:48:50 -0700

    Added views so the user could see content

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Website Built in Node.js with Express, Jade and Stylus
+
+## Screenshot of what the home page looks like
+
+![screenshot](/public/images/home-page-screenshot.png)
--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.end('Hi there!')
+  res.render('index',
+    { title : 'Home' }
+    )
 })
 app.listen(3000)
 
